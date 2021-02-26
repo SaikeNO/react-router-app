410919281751a627da120097a2dface1e0a6b7ee
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2021-02-26 19:28:25 +0100

    little modyfied

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { useSpring, animated } from "react-spring";
+
 import "../styles/Header.css";
-import {Route, Switch} from 'react-router-dom'
 
-import img1 from '../images/header1.jpg' 
-import img2 from '../images/header2.jpg' 
-import img3 from '../images/header3.jpg' 
-
+import img1 from "../images/header1.jpg";
+import img2 from "../images/header2.jpg";
+import img3 from "../images/header3.jpg";
 
 const Header = () => {
-    return ( 
-        <>
-            <Switch>
-                <Route path="/" exact render={() => (
-                    <img src={img1} alt="miasto"></img>
-                )}/>
-                <Route path="/products" render={() => (
-                    <img src={img2} alt="miasto"></img>
-                )}/>
-                <Route path="/contact" render={() => (
-                    <img src={img3} alt="miasto"></img>
-                )}/>
-                <Route path="/admin" render={() => (
-                    <img src={img1} alt="miasto"></img>
-                )}/>
-                <Route render={() => (
-                    <img src={img1} alt="miasto"></img>
-                )}/>
-            </Switch>
-        </>
-     );
-}
- 
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+  return (
+    <>
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <animated.img style={props} src={img1} alt="miasto"></animated.img>
+          )}
+        />
+        <Route
+          path="/products"
+          render={() => <img src={img2} alt="miasto"></img>}
+        />
+        <Route
+          path="/contact"
+          render={() => <img src={img3} alt="miasto"></img>}
+        />
+        <Route
+          path="/admin"
+          render={() => <img src={img1} alt="miasto"></img>}
+        />
+        <Route render={() => <img src={img1} alt="miasto"></img>} />
+      </Switch>
+    </>
+  );
+};
+
 export default Header;
